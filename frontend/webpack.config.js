0937d18126f946b03e9d12ef5f11bf38503b9672
bidanee <dleksql119@gmail.com>
2025-06-12 05:27:14 +0000

    Up

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -30,7 +30,13 @@ module.exports = {
       port: 3000,
       static: path.join(__dirname, 'public'), 
       open: true,
-  },
-
+      watchFiles: {
+        paths: ['src/**/*', 'public/**/*'], // 감지할 파일/폴더 명시
+        options: {
+          usePolling: true, // 폴링 사용을 명시
+          interval: 1000 // 1초(1000ms)마다 파일 변경 여부를 주기적으로 확인
+        }
+      }
+    },
   mode: 'development'
 };
